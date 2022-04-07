ac0e625535378739844c2c3d9b5c613b6733076a
Ervin Puust <90192519+Ervpst@users.noreply.github.com>
2022-04-08 00:03:24 +0100

    junit kodutoo
    junit kodutoo

---
+++ 07_junit/CarObject.java
@@ -0,0 +1,10 @@
+public interface CarObject {
+    String getmark();
+    String getvärv();
+    int getväljalase();
+    int gethind();
+
+    boolean mootoriVahetus();
+    boolean õigeMark();
+    boolean aastadMoodas();
+}
