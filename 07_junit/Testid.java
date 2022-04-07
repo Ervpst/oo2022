75a3878934ddec5b567b937f876f32a70cd67693
Ervin Puust <90192519+Ervpst@users.noreply.github.com>
2022-04-08 00:02:20 +0100

    junit kodutoo
    junit kodutoo

---
+++ 07_junit/Testid.java
@@ -0,0 +1,25 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class Testid {
+
+    @Test
+    public void Kontrolli_kas_auto_mootorit_on_vaja_vahetada() {
+        Car Car = new Car("Lada", "Oranž", 1970, 10000, 2000001);
+        assertEquals(true, Car.mootoriVahetus());
+    }
+
+    @Test
+    public void Kontrolli_kas_auto_mark_on_ikka_Lada() {
+        Car Car = new Car("Lada", "Oranž", 1970, 10000, 2000001);
+        assertEquals(true, Car.õigeMark());
+    }
+    @Test
+    public void Kontrolli_kas_auto_väljalaskmisest_on_moodas_rohkem_kui_40_aastat() {
+        Car Car = new Car("Lada", "Oranž", 1970, 10000, 2000001);
+        assertEquals(true, Car.aastadMoodas());
+    }
+
+
+
+    }
