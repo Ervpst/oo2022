856105e22a8213ff9aa5e6e88fe19eb7781eb349
Ervin Puust <90192519+Ervpst@users.noreply.github.com>
2022-06-06 19:35:11 +0100

    lahendus
    11

---
+++ 11_proovikontrolltoo2/Hammasratas.java
@@ -0,0 +1,10 @@
+
+public class Hammasratas {
+    int hambad;
+    double suurus;
+
+    public Hammasratas(int _hambad, double mm) {
+        this.hambad = _hambad;
+        this.suurus = hambad * mm;
+    }
+}
